ffd626b12dca3a0294c8dc7f8bb2749a2d104fa5
AJ Armstrong <aja@nait.ca>
2014-09-18 14:18:18 -0600

    Demo 3 Created

---
+++ Fall 2014/Demos/Demo 3 - Iteration with Structures/Demo 3 - Iteration with Structures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_3___Iteration_with_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
