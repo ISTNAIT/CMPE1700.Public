0c87c68d58ea153f931c888f6c57c48460489a2b
AJ Armstrong <aja@nait.ca>
2014-09-18 14:58:50 -0600

    Finished Demo

---
+++ Fall 2014/Demos/Demo 3 - Iteration with Structures/Demo 3 - Iteration with Structures/Aviator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_3___Iteration_with_Structures
+{
+    public enum Aviator
+    {
+        Armstrong,
+        Bach,
+        Doolittle,
+        Dornier,
+        Earhart,
+        deHavilland,
+        Hughes,
+        Lindbergh,
+        Stringfellow,
+        Yeager,
+        Wright
+    }
+}
--- Fall 2014/Demos/Demo 3 - Iteration with Structures/Demo 3 - Iteration with Structures/Program.cs
+++ Fall 2014/Demos/Demo 3 - Iteration with Structures/Demo 3 - Iteration with Structures/Program.cs
@@ -10,6 +10,35 @@ namespace Demo_3___Iteration_with_Structures
     {
         static void Main(string[] args)
         {
+            //Training requirement.  I need to have a training flight between
+            //every possible pair of aviators (that is, I want each pilot to fly
+            //one flight with every other pilot).  How to do that?
+
+            //ITERATE!
+
+            //First, I need a array big enough to hold all the possibilities:
+            int pilotCount = Enum.GetValues(typeof(Aviator)).Length;
+            //Total number of combos is X^2-X or X(X-1), whatever
+            PairedFlight[] flights = new PairedFlight[pilotCount*pilotCount - pilotCount];
+            int i = 0;
+
+            foreach (Aviator pilot in Enum.GetValues(typeof(Aviator)))
+            {
+                //Use below if you don't want them to fly together at ALL
+                //Need to change your size of array, too
+                for (Aviator firsto = pilot+1; firsto <= (Aviator)(pilotCount-1); firsto++)
+                    //foreach (Aviator firsto in Enum.GetValues(typeof(Aviator))) //For everybody doubles up
+                    {
+                        if (pilot != firsto)
+                            flights[i++] = new PairedFlight(pilot, firsto); //Woooo! Fancy postscript ++
+                    }
+            }
+
+            foreach (PairedFlight flight in flights)
+                Console.WriteLine(flight);
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
