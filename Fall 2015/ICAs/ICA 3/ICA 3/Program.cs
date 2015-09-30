8ce9ea23815a162bce4c9377e2e3c6219c5e3ac0
AJ Armstrong <aja@nait.ca>
2015-09-30 14:59:04 -0600

    Initial UI completed and tested.

---
+++ Fall 2015/ICAs/ICA 3/ICA 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICA_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong val = 0;  //Value I am generating prime factors for.
+
+            //Confirm I have one argument (exactly)
+            if (args.Count() != 1)
+            {
+                //Print error and exit
+                Utils.PrintError("Invalid number of arguments (" + args.Count() + ")", "", true, true, -1);
+            }
+
+            //Confirm that it is a positive integer
+            try
+            {
+                val = ulong.Parse(args[0]);
+            }
+            catch (Exception e)
+            {
+                Utils.PrintError("Parsing Error on argument \"" + args[0] + "\"", e.Message, true, true, -2);
+            }
+            
+            //If I get here, I have a valid number to check parity of.
+
+        }
+    }
+}
