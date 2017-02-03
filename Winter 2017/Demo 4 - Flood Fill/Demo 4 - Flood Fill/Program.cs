82bc2f1afef14aa7d88363d830cc06a859331873
AJ Armstrong <aja@nait.ca>
2017-02-03 07:41:29 -0700

    Flood Fill Demo

--- Winter 2017/Demo 4 - Flood Fill/Demo 4 - Flood Fill/Program.cs
+++ Winter 2017/Demo 4 - Flood Fill/Demo 4 - Flood Fill/Program.cs
@@ -20,7 +20,7 @@ namespace Demo_4___Flood_Fill
             Point EndPoint = new Point();
             Point FillPoint = new Point();
 
-            Draw.Scale = 20;
+            Draw.Scale = 10;
             for (;;)
             {
                 if (!EndPointPending)
@@ -33,26 +33,56 @@ namespace Demo_4___Flood_Fill
                     if (Draw.GetLastMouseLeftClickScaled(out EndPoint))
                     {
                         EndPointPending = false;
-                        Draw.AddLine(StartPoint.X, StartPoint.Y, EndPoint.X, EndPoint.Y, Color.Blue);
+                        AddBBLineScaled(StartPoint, EndPoint, Color.Blue);
                     }
                 }
                 if (Draw.GetLastMouseRightClickScaled(out FillPoint))
-                    FloodFill(FillPoint);
+                    FloodFill(FillPoint, Color.Red);
             }
             if (Debugger.IsAttached)
                 Console.ReadKey();
         }
 
-        static void FloodFill(Point pos)
+        static void FloodFill(Point pos, Color col)
         {
-            //if (Draw.GetBBPixel(pos.X, pos.Y) != Color.Black) return;
             if (pos.X >= Draw.ScaledWidth || pos.Y >= Draw.ScaledHeight) return;
             if (pos.X < 0 || pos.Y < 0) return;
-            Draw.SetBBScaledPixel(pos.X, pos.Y, Color.Red);
-            FloodFill(new Point(pos.X + 1, pos.Y));
-            FloodFill(new Point(pos.X - 1 , pos.Y));
-            //FloodFill(new Point(pos.X , pos.Y + 1));
-            //FloodFill(new Point(pos.X, pos.Y - 1));
+            Color Pixel = Draw.GetBBPixel(pos.X * Draw.Scale, pos.Y * Draw.Scale);
+            bool IsBlack = (Pixel.R == 0 && Pixel.G == 0 && Pixel.B == 0);
+            if (!IsBlack) return;
+
+            Draw.SetBBScaledPixel(pos.X, pos.Y, col);
+            FloodFill(new Point(pos.X + 1, pos.Y), col);
+            FloodFill(new Point(pos.X - 1, pos.Y), col);
+            FloodFill(new Point(pos.X, pos.Y + 1), col);
+            FloodFill(new Point(pos.X, pos.Y - 1), col);
+        }
+
+        static void AddBBLineScaled(Point start, Point end, Color col)
+        {
+            double m = 0; //Slope
+
+            //Determine axis to iterate across
+
+            if (Math.Abs((double)end.X - (double)start.X) >
+                Math.Abs((double)end.Y - (double)start.Y))
+            {
+                //Iterate across X
+                int rise = end.Y - start.Y;
+                int run = end.X - start.X;
+                m = (double)rise / (double)run;
+                for (int x = start.X; x <= end.X; x += (end.X > start.X) ? 1 : -1)
+                    Draw.SetBBScaledPixel(x, (int)(m * (x - start.X) + start.Y), col);
+            }
+            else
+            {
+                //Iterate across Y
+                int rise = end.X - start.X;
+                int run = end.Y - start.Y;
+                m = (double)rise / (double)run;
+                for (int y = start.Y; y <= end.Y; y += (end.Y > start.Y) ? 1 : -1)
+                    Draw.SetBBScaledPixel((int)(m * (y - start.Y) + start.X),y, col);
+            }
         }
     }
 }
