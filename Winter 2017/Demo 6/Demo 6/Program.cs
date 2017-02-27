77cf4bd6a3fd87b0d51b0040634b9ca10f8f8497
AJ Armstrong <aja@nait.ca>
2017-02-27 09:23:50 -0700

    Vector demo

---
+++ Winter 2017/Demo 6/Demo 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Demo_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v = Vector.Initialize();
+            for(int i = 0; i < 100; ++i)
+            {
+                v = Vector.Add(v, i);
+            }
+
+            foreach (int i in v.Store)
+                Console.Write(i + " ");
+            Console.WriteLine();
+
+            if (Debugger.IsAttached) Console.ReadKey();
+        }
+    }
+}
